ecfe78dd684d133cd94d03b605d7ce5e5c78f7f2
Jeffri Temis <jeffritcf@gmail.com>
2023-02-26 14:35:41 -0500

    third commit

--- calculadora3.py
+++ calculadora3.py
@@ -40,3 +40,31 @@ buttonSuma = ttk.Button(mainframe, text= "+")
 
 buttonIgual = ttk.Button(mainframe, text= "=")
 buttonRaizCuadrada = ttk.Button(mainframe, text= "√")
+
+#Coloca botones en pantalla
+
+buttonParantesis1.grid(column= 0, row= 2)
+buttonParantesis2.grid(column= 1, row=2)
+buttonBorrarTodo.grid(column= 2, row=2)
+buttonBorrar.grid(column= 3, row=2)
+
+button7.grid(column =0, row = 3)
+button8.grid(column =1, row = 3)
+button9.grid(column =2, row = 3)
+buttonDivision.grid(column =3, row = 3)
+
+button4.grid(column =0, row = 4)
+button5.grid(column =1, row = 4)
+button6.grid(column =2, row = 4)
+buttonMultiplicacion.grid(column =3, row = 4)
+
+button1.grid(column =0, row = 5)
+button2.grid(column =1, row = 5)
+button3.grid(column =2, row = 5)
+buttonSuma.grid(column =3, row = 5)
+
+button0.grid(column =0, row = 6, columnspan = 2, sticky = (W , E))
+buttonPunto.grid(column =2, row = 6)
+buttonResta.grid(column =3, row = 6)
+
+root.mainloop()
